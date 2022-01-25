1deb4aaa9db9358e4e79c7d3e89dc02fa973a0db
Renato Diniz <renatodiniz25@gmail.com>
2022-01-24 22:23:23 -0300

    Add operation ok

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const dividekey = document.getElementById('dividekey');
 const multiplykey =  document.getElementById('multiplykey');
 const subtractkey = document.getElementById('subtractkey');
 const addkey = document.getElementById('addkey');
-const qualskey = document.getElementById('equalskey');
+const equalskey = document.getElementById('equalskey');
 const decimalkey = document.getElementById('decimalkey');
 
 const zero = document.getElementById('zero');
@@ -23,10 +23,12 @@ const nine = document.getElementById('nine');
 let currentop = 0;
 let currentvalue = 0;
 let priorvalue = 0;
+let operating = false;
 
 screen.innerText = currentvalue;
 
 ac.addEventListener('click', clearCalculator);
+equalskey.addEventListener('click', operate);
 
 zero.addEventListener('click', inputKey);
 one.addEventListener('click', inputKey);
@@ -43,7 +45,10 @@ addkey.addEventListener('click', addOp);
 function inputKey(key) {
   if (currentvalue == '0') {
     currentvalue = '';
+  } else if (operating) {
+    currentvalue = '';
   }
+  operating = false;
   currentvalue += key.currentTarget.innerText;
   return screen.innerText = currentvalue;
 }
@@ -53,15 +58,25 @@ function clearCalculator() {
   currentvalue = 0;
   priorvalue = 0;
   screen.innerText = currentvalue;
-
 }
+
+function operate() {
+  switch (currentop) {
+    case 1:
+      currentvalue = add(currentvalue, priorvalue);
+      screen.innerText = currentvalue;
+      break;
+  }
+}
+
 function addOp() {
   currentop = 1;
-  priordisplayvalue = currentdisplayvalue;
+  priorvalue = currentvalue;
+  operating = true;
 }
 
 function add(a, b) {
-  return a + b;
+  return +a + +b;
 }
 
 function subtract(a, b) {
